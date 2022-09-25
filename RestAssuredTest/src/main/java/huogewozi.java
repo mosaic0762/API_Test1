86784f03734fa27a04a47ecc1576d91c6cd9fc96
“mosaic0762” <571522671@qq.com>
2022-09-26 01:10:56 +0800

    分支提交第二次

---
+++ RestAssuredTest/src/main/java/huogewozi.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+/*
+ * @Description： //TODO
+ * @Author: 陶春有
+ * @Date: 2022-09-26 01:09
+ **/
+public class huogewozi {
+
+
+    public static void main(String[] args) {
+        System.out.println("第二次分支提交");
+    }
+}
