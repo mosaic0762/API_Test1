78e535325bb0b966f9bcaa0259557f57bdc3a734
“mosaic0762” <571522671@qq.com>
2022-09-26 01:28:41 +0800

    TCY2分支提交第一次

--- RestAssuredTest/src/test/java/shangguigu.java
+++ RestAssuredTest/src/test/java/shangguigu.java
@@ -1,7 +1,16 @@
+import org.testng.annotations.Test;
+
 /*
  * @Description： //TODO
  * @Author: 陶春有
  * @Date: 2022-09-26 01:12
  **/
 public class shangguigu {
+
+    @Test
+    public void testMethod(){
+
+
+
+    }
 }
